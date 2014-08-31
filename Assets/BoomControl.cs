1e903dbb1a919ed01c7faaf5aa42abc005655ec5
vbetter <admin@admindeMac-mini.local>
2014-08-31 16:38:38 +0800

    导弹追踪完整包

---
+++ Assets/BoomControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoomControl : MonoBehaviour {
+	public Vector3 force;
+	private Rigidbody2D rigid;
+	// Use this for initialization
+	void Start () {
+		rigid=gameObject.GetComponent<Rigidbody2D>();
+		rigid.AddForce(force);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+
+	}
+}
